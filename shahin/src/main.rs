e7517f5e014afeb68c98a6137947f866f3186158
MuhammedShahin <mohamedshahin796@gmail.com>
2024-06-05 03:44:20 +0300

    shahin: reading input file and calc freqs

--- shahin/src/main.rs
+++ shahin/src/main.rs
@@ -1,20 +1,50 @@
 pub mod huffman;
 use std::collections::BinaryHeap;
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+
+use huffman::Node;
 
 fn main() {
-    println!("Hello, world!");
 
-    let mut pq = BinaryHeap::new();
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
 
-    pq.push(huffman::Node{ character:'A', freq: 8});
-    pq.push(huffman::Node{ character:'2', freq: 9});
-    pq.push(huffman::Node{ character:'X', freq: 89});
-    pq.push(huffman::Node{ character:'T', freq: 23});
-    pq.push(huffman::Node{ character:'S', freq: 10});
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+
+    let mut symboles_freq = HashMap::new();
+
+    for character in contents.chars() {
+        let count = symboles_freq.entry(character).or_insert(0);
+        *count += 1;
+    }
     
-
+    let mut pq = BinaryHeap::new();
+    for (key, value) in &symboles_freq {
+        pq.push(huffman::Node { character: *key, freq: *value });
+    }
+    
     while let Some(node) = pq.pop() {
         println!("freq: {}, char: {}", node.freq, node.character);
     }
-    
+
+}
+
+
+fn build_huffman_tree(symbols: BinaryHeap<Node>)  {
+    /*
+    * pop the first 2 nodes from the queue
+    *
+    * create parnt node with the sum of the 2 childs nodes
+    *
+    * insert the parnt node in the queue
+    *
+    * repeat until you have one node inside the queue
+    */
 }
