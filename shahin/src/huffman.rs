1da0d1aea9a0ba56fdde9df1485cf5c93ddd1360
MuhammedShahin <mohamedshahin796@gmail.com>
2024-06-07 12:33:51 +0300

    shahin: building huffman tree & huffman lookup

--- shahin/src/huffman.rs
+++ shahin/src/huffman.rs
@@ -1,19 +1,18 @@
 use std::cmp::Ordering;
 
-
 #[derive(Eq, PartialEq)]
 pub struct Node {
-    pub(crate) character: char,
+    pub(crate) character: Option<char>,
     pub(crate) freq: u32,
+    pub(crate) left_node: Option<Box<Node>>,
+    pub(crate) right_node: Option<Box<Node>>,
 }
-
 
 impl Ord for Node {
     fn cmp(&self, other: &Self) -> Ordering {
         other.freq.cmp(&self.freq)
     }
 }
-
 
 impl PartialOrd for Node {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
--- shahin/src/main.rs
+++ shahin/src/main.rs
@@ -6,45 +6,82 @@ use std::fs;
 
 use huffman::Node;
 
-fn main() {
-
+fn read_input_file() -> String {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
 
     println!("In file {}", file_path);
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
+    return contents;
+}
 
+
+fn analyz_symboles(contents: String) -> BinaryHeap<Node> {
     let mut symboles_freq = HashMap::new();
 
     for character in contents.chars() {
         let count = symboles_freq.entry(character).or_insert(0);
         *count += 1;
     }
-    
-    let mut pq = BinaryHeap::new();
+
+    let mut symboles_queue = BinaryHeap::new();
     for (key, value) in &symboles_freq {
-        pq.push(huffman::Node { character: *key, freq: *value });
-    }
-    
-    while let Some(node) = pq.pop() {
-        println!("freq: {}, char: {}", node.freq, node.character);
+        symboles_queue.push(Node {
+            character: Some(*key),
+            freq: *value,
+            left_node: None,
+            right_node: None,
+        });
     }
 
+    return symboles_queue;
 }
 
 
-fn build_huffman_tree(symbols: BinaryHeap<Node>)  {
-    /*
-    * pop the first 2 nodes from the queue
-    *
-    * create parnt node with the sum of the 2 childs nodes
-    *
-    * insert the parnt node in the queue
-    *
-    * repeat until you have one node inside the queue
-    */
+fn build_huffman_tree(mut symbols: BinaryHeap<Node>) -> Option<Node> {
+    while symbols.len() > 1 {
+        let left_node = symbols.pop().unwrap();
+        let right_node = symbols.pop().unwrap();
+
+        let parent_node = Node {
+            character: None,
+            freq: left_node.freq + right_node.freq,
+            left_node: Some(Box::new(left_node)),
+            right_node: Some(Box::new(right_node)),
+        };
+
+        symbols.push(parent_node);
+    }
+
+    return symbols.pop();
+}
+
+
+fn build_huffman_lookup(node: &Node, prefix: String, hufmman_lookup: &mut HashMap<char, String>) {
+    if let Some(character) = node.character {
+        hufmman_lookup.insert(character, prefix);
+    } else {
+        if let Some(ref left_node) = node.left_node {
+            build_huffman_lookup(left_node, format!("{}0", prefix), hufmman_lookup);
+        }
+        if let Some(ref right_node) = node.right_node {
+            build_huffman_lookup(right_node, format!("{}1", prefix), hufmman_lookup);
+        }
+    }
+}
+
+
+fn main() {
+    let contents = read_input_file();
+    let symboles_queue = analyz_symboles(contents);
+
+    let mut huffman_lookup = HashMap::new();
+    if let Some(huffman_tree) = build_huffman_tree(symboles_queue) {
+        build_huffman_lookup(&huffman_tree, String::new(), &mut huffman_lookup);
+    }
+    for (character, prefix) in huffman_lookup {
+        println!("character: {:?}, prefix: {}", character, prefix);
+    }
 }
