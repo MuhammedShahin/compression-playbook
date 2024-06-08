eb21d72e5a74998d17c8f5eb3137f155aa5c5924
Ahmed Essam <aessam.dahy@gmail.com>
2024-06-08 13:25:45 +0300

    essam: basic compression and decompression

--- essam/src/huffman.rs
+++ essam/src/huffman.rs
@@ -6,20 +6,21 @@ pub struct HuffmanTree {
     num_symbols: usize,
 }
 
+#[derive(Debug, Default)]
 pub struct HuffmanTable {
-    codes: Vec<PrefixCode>,
+    pub codes: Vec<PrefixCode>,
 }
 
 #[derive(Default, Debug, Clone, Copy)]
 pub struct PrefixCode {
-    code: u64,
-    length: u8,
+    pub code: u32,
+    pub length: u8,
 }
 
 #[derive(Debug, Clone, Copy)]
 pub struct WalkIterator {
     code: PrefixCode,
-    idx: u32,
+    idx: usize,
 }
 
 #[derive(Debug, Error)]
@@ -31,11 +32,11 @@ pub enum WalkError {
     InvalidIndex,
 }
 
+#[derive(Copy, Clone)]
 struct Node {
     freq: u32,
     left: Option<u32>,
     right: Option<u32>,
-    parent: Option<u32>, // We might not need it
 }
 
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
@@ -47,14 +48,24 @@ struct HeapEntry {
 impl PrefixCode {
     fn update(prefix_code: PrefixCode, bit: bool) -> PrefixCode {
         PrefixCode {
-            code: (prefix_code.code << 1) | (bit as u64),
+            code: prefix_code.code | ((bit as u32) << (prefix_code.length)),
             length: prefix_code.length + 1,
         }
     }
 }
 
+impl std::fmt::Display for PrefixCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "PrefixCode {{ code: {:b}, length: {} }}",
+            self.code, self.length
+        )
+    }
+}
+
 impl HuffmanTree {
-    pub fn build(freqs: Vec<u32>) -> HuffmanTree {
+    pub fn build(freqs: &Vec<u32>) -> HuffmanTree {
         let num_symbols = freqs.len();
         let capacity = 2 * num_symbols - 1;
 
@@ -70,7 +81,6 @@ impl HuffmanTree {
                 freq: *freq,
                 left: None,
                 right: None,
-                parent: None,
             });
 
             heap.push(std::cmp::Reverse(HeapEntry {
@@ -92,8 +102,7 @@ impl HuffmanTree {
             let internal_node = Node {
                 freq: nodes[node1_idx].freq + nodes[node2_idx].freq,
                 left: Some(entry1.0.idx),
-                right: Some(entry1.0.idx),
-                parent: None,
+                right: Some(entry2.0.idx),
             };
 
             let internal_node_idx = nodes.len() as u32;
@@ -102,22 +111,18 @@ impl HuffmanTree {
                 idx: internal_node_idx,
             }));
             nodes.push(internal_node);
-
-            // Set the parent of the least two nodes to the new created internal node.
-            nodes[node1_idx].parent = Some(internal_node_idx);
-            nodes[node2_idx].parent = Some(internal_node_idx);
         }
 
         HuffmanTree { nodes, num_symbols }
     }
 
-    fn is_leaf_node(&self, idx: u32) -> bool {
+    fn is_leaf_node(&self, idx: usize) -> bool {
         (idx as usize) < self.num_symbols
     }
 
     pub fn create_walk_iter(&self) -> WalkIterator {
         WalkIterator {
-            idx: (self.nodes.len() - 1) as u32,
+            idx: self.nodes.len() - 1,
             code: PrefixCode::default(),
         }
     }
@@ -130,14 +135,81 @@ impl HuffmanTree {
         Ok(WalkIterator {
             code: PrefixCode::update(iter.code, bit),
             idx: if bit {
-                self.nodes[iter.idx as usize].right.unwrap()
+                self.nodes[iter.idx].right.unwrap() as usize
             } else {
-                self.nodes[iter.idx as usize].left.unwrap()
+                self.nodes[iter.idx].left.unwrap() as usize
             },
         })
     }
 }
 
 impl HuffmanTable {
+    fn build_impl(tree: &HuffmanTree, idx: usize, code: PrefixCode, table: &mut HuffmanTable) {
+        if tree.is_leaf_node(idx) {
+            table.codes[idx] = code;
+            return;
+        }
 
+        let node = &tree.nodes[idx];
+
+        if let Some(left) = node.left {
+            Self::build_impl(tree, left as usize, PrefixCode::update(code, false), table);
+        }
+
+        if let Some(right) = node.right {
+            Self::build_impl(tree, right as usize, PrefixCode::update(code, true), table);
+        }
+    }
+
+    pub fn code(&self, symbol: usize) -> PrefixCode {
+        self.codes[symbol]
+    }
+}
+
+impl From<&HuffmanTree> for HuffmanTable {
+    fn from(tree: &HuffmanTree) -> Self {
+        let mut table = HuffmanTable { codes: Vec::new() };
+        // Is there a better way to directly initialize the vector in the above line?
+        table.codes.resize(tree.num_symbols, PrefixCode::default());
+
+        Self::build_impl(
+            tree,
+            tree.nodes.len() - 1,
+            PrefixCode::default(),
+            &mut table,
+        );
+
+        return table;
+    }
+}
+
+// TODO
+#[allow(unused_variables)]
+impl From<&HuffmanTable> for HuffmanTree {
+    fn from(table: &HuffmanTable) -> Self {
+        let num_symbols = table.codes.len();
+        let capacity = 2 * num_symbols - 1;
+
+        let mut nodes = Vec::<Node>::new();
+        nodes.resize(
+            capacity,
+            Node {
+                freq: 0,
+                left: None,
+                right: None,
+            },
+        );
+
+        //  The last node is the root
+        let root_idx = nodes.len() - 1;
+        let alloc_idx = nodes.len() - 2;
+
+        for code in table.codes.iter() {
+            let crawler_idx = root_idx;
+
+            for bit_idx in 0..code.length {}
+        }
+
+        HuffmanTree { nodes, num_symbols }
+    }
 }
--- essam/src/main.rs
+++ essam/src/main.rs
@@ -1,5 +1,199 @@
-use essam::huffman;
+use byteorder::{ByteOrder, LittleEndian, ReadBytesExt, WriteBytesExt};
+use clap::Parser;
+use essam::huffman::{HuffmanTable, HuffmanTree, PrefixCode};
+use std::fs::File;
+use std::io::Seek;
+use std::io::{BufReader, BufWriter};
+use std::io::{Read, Write};
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Clone, clap::Args)]
+struct OperationArgs {
+    input_path: String,
+    output_path: String,
+}
+
+#[derive(Debug, Clone, clap::Subcommand)]
+enum Operation {
+    Compress(OperationArgs),
+    Decompress(OperationArgs),
+}
+
+#[derive(Debug, clap::Parser)]
+struct Args {
+    #[command(subcommand)]
+    op: Operation,
+}
+
+const EOF: usize = 256;
+
+fn create_stats(buf_reader: &mut impl Read) -> anyhow::Result<Vec<u32>> {
+    let mut stats = Vec::new();
+    stats.resize(257, 0);
+    stats[EOF] = 1;
+
+    let mut buffer = [0; 256];
+
+    while let Ok(num_read_bytes) = buf_reader.read(&mut buffer) {
+        for byte in &buffer[0..num_read_bytes] {
+            stats[*byte as usize] += 1;
+        }
+
+        if num_read_bytes == 0 {
+            break;
+        }
+    }
+
+    Ok(stats)
+}
+
+fn write_huffman_table(
+    writer: &mut impl Write,
+    huffman_table: &HuffmanTable,
+) -> anyhow::Result<()> {
+    writer.write_u16::<LittleEndian>(huffman_table.codes.len() as u16)?;
+
+    // This can be more efficient by writing only the required bits.
+    for code in huffman_table.codes.iter() {
+        writer.write_u8(code.length)?;
+        writer.write_u32::<LittleEndian>(code.code)?;
+    }
+
+    Ok(())
+}
+
+fn read_huffman_table(reader: &mut impl Read) -> anyhow::Result<HuffmanTable> {
+    let mut table = HuffmanTable::default();
+
+    let table_len = reader.read_u16::<LittleEndian>()? as usize;
+    table.codes.resize(table_len, PrefixCode::default());
+
+    for code in table.codes.iter_mut() {
+        code.length = reader.read_u8()?;
+        code.code = reader.read_u32::<LittleEndian>()?;
+    }
+
+    Ok(table)
+}
+
+fn compress(input_path: String, output_path: String) -> anyhow::Result<()> {
+    let input_file = File::open(input_path)?;
+    let output_file = File::create(output_path)?;
+
+    let mut buf_reader = BufReader::new(input_file);
+    let mut buf_writer = BufWriter::new(output_file);
+
+    let stats = create_stats(&mut buf_reader)?;
+    buf_reader.rewind()?;
+
+    let tree = HuffmanTree::build(&stats);
+    let table = HuffmanTable::from(&tree);
+
+    write_huffman_table(&mut buf_writer, &table)?;
+
+    let mut byte = [0; 1];
+
+    let mut pending: u32 = 0;
+    let mut pending_length: u8 = 0;
+
+    let mut write_symbol = |symbol: usize| -> anyhow::Result<()> {
+        let prefix_code = table.code(symbol);
+
+        if pending_length + prefix_code.length < 32 {
+            pending = pending | (prefix_code.code << pending_length);
+            pending_length = pending_length + prefix_code.length;
+        } else {
+            let to_write = pending | (prefix_code.code.overflowing_shl(pending_length as u32)).0;
+            buf_writer.write_u32::<LittleEndian>(to_write)?;
+
+            pending_length = (pending_length + prefix_code.length) - 32;
+            pending = prefix_code.code >> (prefix_code.length - pending_length);
+        }
+
+        Ok(())
+    };
+
+    while let Ok(num_read_bytes) = buf_reader.read(&mut byte) {
+        if num_read_bytes == 0 {
+            break;
+        }
+        write_symbol(byte[0] as usize)?;
+    }
+    write_symbol(EOF)?;
+
+    // Write the last few
+    if pending_length != 0 {
+        let mut bytes = [0; 4];
+        LittleEndian::write_u32(&mut bytes, pending);
+
+        let num_bytes = ((pending_length + 7) / 8) as usize;
+        buf_writer.write(&bytes[0..num_bytes])?;
+    }
+
+    Ok(())
+}
+
+fn match_prefix_code(code: u32, table: &HuffmanTable) -> Option<usize> {
+    for (idx, prefix_code) in table.codes.iter().enumerate() {
+        let (mask, _) = (!(0 as u32)).overflowing_shr(32 - prefix_code.length as u32);
+        if (code & mask) == prefix_code.code {
+            return Some(idx);
+        }
+    }
+
+    None
+}
+
+fn decompress(input_path: String, output_path: String) -> anyhow::Result<()> {
+    let input_file = File::open(input_path)?;
+    let output_file = File::create(output_path)?;
+
+    let mut buf_reader = BufReader::new(input_file);
+    let mut buf_writer = BufWriter::new(output_file);
+
+    let table = read_huffman_table(&mut buf_reader)?;
+
+    let mut pending: u64 = 0;
+    let mut pending_length: u8 = 0;
+
+    let mut byte = [0; 1];
+    let mut done = false;
+
+    while let Ok(num_read_bytes) = buf_reader.read(&mut byte) {
+        if num_read_bytes == 0 {
+            break;
+        }
+
+        pending = pending | ((byte[0] as u64) << pending_length);
+        pending_length = pending_length + 8;
+
+        while let Some(symbol_idx) = match_prefix_code(pending as u32, &table) {
+            if table.codes[symbol_idx].length > pending_length {
+                break;
+            }
+            if symbol_idx == EOF {
+                done = true;
+                break;
+            }
+
+            buf_writer.write_u8(symbol_idx as u8)?;
+
+            pending_length -= table.codes[symbol_idx].length;
+            pending = pending >> table.codes[symbol_idx].length;
+        }
+
+        if done {
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    match args.op {
+        Operation::Compress(args) => compress(args.input_path, args.output_path),
+        Operation::Decompress(args) => decompress(args.input_path, args.output_path),
+    }
 }
